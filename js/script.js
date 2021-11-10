26064126c4c8eaadd700a8a276d2a33ffe0c9269
Alex <adrdra88@gmail.com>
2021-11-10 19:17:09 +0100

    style.2

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ const email = document.getElementById('email').value;
 
 const emailReg = ["alex@banana","piero@banana","gianni@banana", "monica@banana","gaetano@banana", "federico@banana","luca@banana", "peppe@banana","gino@banana", "sarbina@banana","ugo@banana", "ego@banana"];
 
-    let controllo = false;
+let controllo = false;
 
 for(i = 0; i <emailReg.length; i++){
     if(emailReg[i] == email){
@@ -22,12 +22,11 @@ for(i = 0; i <emailReg.length; i++){
 }
 
 if (controllo){    
-    let result = document.getElementById('result').innerHTML += ' é riconosciuta';
+    let result = document.getElementById('result').innerHTML = ' é riconosciuta';
 }else{
-    let result = document.getElementById('result').innerHTML += ' non é riconosciuta';
+    let result = document.getElementById('result').innerHTML = ' non é riconosciuta';
 }
 
-console.log(result)
 })
 
 
